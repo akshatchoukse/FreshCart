b0bd10ade8874335ba47bdbf7fa2807780cae469
akshatchoukse <choukseakshat@gmail.com>
2025-08-10 16:22:02 +0530

    Banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
+import { Route, Routes, useLocation } from 'react-router-dom'
 
 function App() {
+  const isSellerPath = useLocation().pathname.includes("seller");
   return (
     <div>
-      <Navbar/>
+      {isSellerPath ? null : <Navbar/>}
+             <div className={`${isSellerPath? "" :"px-6 md:px-16 lg:px-24 xl:px-32"}`}>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+        </Routes>
+      </div>
     </div>
   )
 }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Banner from '../Components/Banner'
+
+function Home() {
+  return (
+    <div className='mt-10 '>
+        <Banner/>
+
+    </div>
+  )
+}
+
+export default Home
